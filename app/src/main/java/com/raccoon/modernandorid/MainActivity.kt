2a2bfa1f15b220900eb053d55e74627e0559156f
raccoon86 <onehub86@gmail.com>
2022-09-02 18:43:23 +0900

    하단 네비게이션 메뉴 생성

--- app/src/main/java/com/raccoon/modernandorid/MainActivity.kt
+++ app/src/main/java/com/raccoon/modernandorid/MainActivity.kt
@@ -2,10 +2,53 @@ package com.raccoon.modernandorid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.raccoon.modernandorid.databinding.ActivityMainBinding
+import com.raccoon.modernandorid.ui.view.FavoriteFragment
+import com.raccoon.modernandorid.ui.view.SearchFragment
+import com.raccoon.modernandorid.ui.view.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
+
+        setupBottomNavigationView()
+
+        // 앱이 처음 생성 됐을때만 메뉴가 생성
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_serach
+        }
     }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_serach -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingsFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/raccoon/modernandorid/ui/view/SettingsFragment.kt
@@ -0,0 +1,27 @@
+package com.raccoon.modernandorid.ui.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.raccoon.modernandorid.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
